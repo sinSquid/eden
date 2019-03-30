88369abe8588959d33ab322243bb1ef7159679c7
sinSquid.you <youlqsoft@gmail.com>
2019-03-30 13:59:29 +0800

    新增permission校验配置

---
+++ src/lib/config/Permission.js
@@ -0,0 +1,27 @@
+const NavPermission = {
+  home: 1,
+  message: 1,
+  userInfo: 1
+}
+
+const permissionMapping = {
+  PERMISSION_HOME: 1, // 主页
+  PERMISSION_MESS_ELE: 20, // 饿了么
+  PERMISSION_MESS_Zhihu: 21, // 知乎
+  PERMISSION_INFO_SELF: 30, // 个人信息
+  PERMISSION_INFO_: 31 // 信息汇总
+}
+
+const rolePermission = {
+  1: true,
+  20: true,
+  21: false,
+  30: true,
+  31: true
+}
+
+export {
+  NavPermission,
+  permissionMapping,
+  rolePermission
+}
---
+++ src/views/login/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="login-container">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'tp-login',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
