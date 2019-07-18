50af2ac394cb807f77e4fef281f5c31c5fe52253
sinSquid.you <youlqsoft@gmail.com>
2019-07-18 18:18:48 +0800

    日常提交

--- src/views/health/doctor/info/info-form.vue
+++ src/views/health/doctor/info/info-form.vue
@@ -113,7 +113,7 @@ export default {
         });
     },
     resetForm() {
-      this.$refs.doctorForm.resetForm();
+      this.$refs.doctorForm.resetFields();
     },
   },
 };
---
+++ src/views/health/practice/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <el-button @click="loadRender">Start</el-button>
+    <ul id="bigshan">
+    </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'healPractice',
+  data() {
+    return {
+
+    };
+  },
+  methods: {
+    sliceGroup(amount) { // amount more than 10k
+      const limit = 500;
+      const num = _.ceil(amount / limit) - 1;
+      const max = _.max(num * limit, amount);
+      const arr = [];
+      for (let i = 0; i < num; i += 1) {
+        arr.push({ s: i, e: i * limit - 1 });
+      }
+      arr.push({ s: num * limit, e: max });
+      return arr;
+    },
+    async loadAllRender() {
+      /* const ul = document.querySelector('#bigshan');
+      const aims = document.createElement('li');
+      const group = this.sliceGroup(100000);
+      for (const gp of group) {
+        await this.loadRender(ul, aims, gp);
+      } */
+    },
+    loadRender(target, source, area) {
+      let { s } = area;
+      const { e } = area;
+      const aims = source;
+      while (s < e) {
+        aims.innerHTML = s;
+        target.appendChild(aims);
+        s += 1;
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
