32f0945c66af99b9d0379cf12e109790fb82b5df
sinSquid.you <youlqsoft@gmail.com>
2019-07-24 13:21:03 +0800

    使用generator完成渲染10w数据量

--- src/views/health/doctor/practice/test/test.vue
+++ src/views/health/doctor/practice/test/test.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <el-button @click="loadAllRender">Start</el-button>
+    <el-button @click="scheduler">Gen</el-button>
     <ul id="bigshan">
     </ul>
   </div>
@@ -58,6 +59,62 @@ export default {
       if (s < e) {
         window.requestAnimationFrame(this.loadRender);
       }
+    },
+    genLoadRender(target, area) {
+      let { s } = area;
+      const { e } = area;
+      const fragment = document.createDocumentFragment();
+      while (s <= e) {
+        const aims = document.createElement('li');
+        aims.innerHTML = s;
+        fragment.appendChild(aims);
+        s += 1;
+      }
+      if (target) {
+        target.append(fragment);
+      } else {
+        const parent = document.getElementById('bigshan');
+        parent.appendChild(fragment);
+      }
+      const aims = document.createElement('li');
+      aims.innerHTML = e;
+      return aims;
+    },
+    genSliceGroup(amount) { // amount more than 10k
+      const limit = 10;
+      const num = _.ceil(amount / limit) - 1;
+      const max = _.max([num * limit, amount]);
+      const arr = [];
+      for (let i = 0; i < num; i += 1) {
+        arr.push({ index: i, s: i * limit, e: (i + 1) * limit - 1 });
+      }
+      arr.push({ index: num, s: num * limit, e: max });
+      return arr;
+    },
+    * genLoadAllRender() {
+      const groups = this.genSliceGroup(500000);
+      const scheduler = [];
+      try {
+        for (const gp of groups) {
+          scheduler[gp.index] = yield this.genLoadRender(scheduler[gp.index - 1], gp);
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    },
+    schedulerLoad(task) {
+      setTimeout(() => {
+        const taskObj = task.next(task.value);
+        if (!taskObj.done) {
+          const aims = _.cloneDeep(task);
+          aims.value = taskObj.value;
+          this.schedulerLoad(task);
+        }
+      }, 0);
+    },
+    scheduler() {
+      const task = this.genLoadAllRender();
+      this.schedulerLoad(task);
     },
   },
 };
