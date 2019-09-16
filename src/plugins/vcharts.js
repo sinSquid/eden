39fe86edbc7afb21d70f502702b2ca6b6d423273
sinSquid <youlqsoft@gmail.com>
2019-09-16 15:07:36 +0800

    v-charts图形缺失注册

--- src/plugins/vcharts.js
+++ src/plugins/vcharts.js
@@ -7,8 +7,9 @@ import VeHistogram from 'v-charts/lib/histogram.common.min';
 import VeScatter from 'v-charts/lib/scatter.common.min';
 import VeRadar from 'v-charts/lib/radar.common.min';
 import VeLiquidfill from 'v-charts/lib/liquidfill.common.min';
+import VePie from 'v-charts/lib/pie.common.min';
 
-const component = [VeLine, VeChart, VeHistogram, VeScatter, VeRadar, VeLiquidfill];
+const component = [VeLine, VeChart, VeHistogram, VeScatter, VeRadar, VeLiquidfill, VePie];
 (() => {
   for (const com of component) {
     Vue.component(com.name, com);
